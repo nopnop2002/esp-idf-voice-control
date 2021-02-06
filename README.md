1a8f730b55d61471db48c0d2114e25748434a577
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-02-07 08:57:51 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # esp-idf-voice-control
-Demonstration of voice control of esp32
+Demonstration of voice control of esp32.   
+No hardware other than ESP32 and smartphone is required.   
+
+# Installation for ESP32
+
+```
+git clone https://github.com/nopnop2002/esp-idf-st7789
+cd esp-idf-st7789/
+idf.py set-target esp32
+idf.py flash monitor -p PORT
+```
+
+# Installation for ESP32-S2
+
+```
+git clone https://github.com/nopnop2002/esp-idf-st7789
+cd esp-idf-st7789/
+idf.py set-target esp32s2
+idf.py flash monitor -p PORT
+
+# Installation of smartphone app
+
+- Install [this](https://play.google.com/store/apps/details?id=appinventor.ai_cempehlivan92.Arduino_Sesli_Kontrol) app on your smartphone.
+ This app is an app that converts voice to text and sends it by Bluetooth2.0 SPP.   
+
+- After installing the app on your smartphone, add ESP_SPP_ACCEPTOR to your pairing device.
+
+- After launching the app, select "ESP_SPP_ACCEPTOR" from the CONNECT menu to pair.
+![Arduino_Voice_Control-1](https://user-images.githubusercontent.com/6020549/107132341-64bedf80-6921-11eb-99d7-e6e032c78b3a.JPG)
+
+![Arduino_Voice_Control-2](https://user-images.githubusercontent.com/6020549/107132344-67b9d000-6921-11eb-8824-8e6515a8d025.JPG)
+
+- From the LANGUAGE menu, select your country so that you can recognize your native language.
+![Arduino_Voice_Control-3](https://user-images.githubusercontent.com/6020549/107132346-6a1c2a00-6921-11eb-88f2-981d418b582f.JPG)
+
+- When you tap the microphone and speak, the spoken words will be displayed on the ESP32.
+![Arduino_Voice_Control-4](https://user-images.githubusercontent.com/6020549/107132349-6dafb100-6921-11eb-8a85-5a6c40896086.JPG)
+![esp-idf-voice-control](https://user-images.githubusercontent.com/6020549/107132361-920b8d80-6921-11eb-800f-58bdb1ad5963.jpg)
+
+
---
+++ main/main.c
@@ -0,0 +1,250 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+#include "esp_bt_device.h"
+#include "esp_spp_api.h"
+
+#include "time.h"
+#include "sys/time.h"
+
+#define SPP_TAG "SPP_ACCEPTOR"
+#define MAIN_TAG "MAIN"
+#define SPP_SERVER_NAME "SPP_SERVER"
+#define EXAMPLE_DEVICE_NAME "ESP_SPP_ACCEPTOR"
+
+static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
+
+//static struct timeval time_new, time_old;
+//static long data_num = 0;
+
+static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_AUTHENTICATE;
+static const esp_spp_role_t role_slave = ESP_SPP_ROLE_SLAVE;
+
+#define CMD_OPEN		100
+#define CMD_RECEIVE		300
+#define CMD_CLOSE		400
+
+typedef struct {
+	uint32_t sppHandle;
+	uint16_t command;
+	size_t	 length;
+	uint8_t  payload[64];
+	TaskHandle_t taskHandle;
+} CMD_t;
+
+QueueHandle_t xQueueCmd;
+
+static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+	CMD_t cmdBuf;
+	switch (event) {
+	case ESP_SPP_INIT_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_INIT_EVT");
+		esp_bt_dev_set_device_name(EXAMPLE_DEVICE_NAME);
+		esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+		esp_spp_start_srv(sec_mask,role_slave, 0, SPP_SERVER_NAME);
+		break;
+	case ESP_SPP_DISCOVERY_COMP_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_DISCOVERY_COMP_EVT");
+		break;
+	case ESP_SPP_OPEN_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_OPEN_EVT");
+		break;
+	case ESP_SPP_CLOSE_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_CLOSE_EVT");
+		cmdBuf.command = CMD_CLOSE;
+		xQueueSend(xQueueCmd, &cmdBuf, 0);
+		break;
+	case ESP_SPP_START_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_START_EVT");
+		break;
+	case ESP_SPP_CL_INIT_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_CL_INIT_EVT");
+		break;
+	case ESP_SPP_DATA_IND_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_DATA_IND_EVT len=%d handle=%d",
+				 param->data_ind.len, param->data_ind.handle);
+		esp_log_buffer_hex("data_ind",param->data_ind.data,param->data_ind.len);
+
+		cmdBuf.sppHandle = param->data_ind.handle;
+		cmdBuf.command = CMD_RECEIVE;
+		strcpy((char *)cmdBuf.payload, (char *)param->data_ind.data);
+		cmdBuf.payload[param->data_ind.len] = 0;
+		cmdBuf.length = param->data_ind.len;
+		xQueueSend(xQueueCmd, &cmdBuf, 0);
+		break;
+	case ESP_SPP_CONG_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT");
+		break;
+	case ESP_SPP_WRITE_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_WRITE_EVT");
+		break;
+	case ESP_SPP_SRV_OPEN_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_OPEN_EVT");
+		cmdBuf.command = CMD_OPEN;
+		xQueueSend(xQueueCmd, &cmdBuf, 0);
+		//gettimeofday(&time_old, NULL);
+		break;
+	case ESP_SPP_SRV_STOP_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_STOP_EVT");
+		break;
+	case ESP_SPP_UNINIT_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_SPP_UNINIT_EVT");
+		break;
+	default:
+		break;
+	}
+}
+
+void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+	switch (event) {
+	case ESP_BT_GAP_AUTH_CMPL_EVT:{
+		if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+			ESP_LOGI(SPP_TAG, "authentication success: %s", param->auth_cmpl.device_name);
+			esp_log_buffer_hex(SPP_TAG, param->auth_cmpl.bda, ESP_BD_ADDR_LEN);
+		} else {
+			ESP_LOGE(SPP_TAG, "authentication failed, status:%d", param->auth_cmpl.stat);
+		}
+		break;
+	}
+	case ESP_BT_GAP_PIN_REQ_EVT:{
+		ESP_LOGI(SPP_TAG, "ESP_BT_GAP_PIN_REQ_EVT min_16_digit:%d", param->pin_req.min_16_digit);
+		if (param->pin_req.min_16_digit) {
+			ESP_LOGI(SPP_TAG, "Input pin code: 0000 0000 0000 0000");
+			esp_bt_pin_code_t pin_code = {0};
+			esp_bt_gap_pin_reply(param->pin_req.bda, true, 16, pin_code);
+		} else {
+			ESP_LOGI(SPP_TAG, "Input pin code: 1234");
+			esp_bt_pin_code_t pin_code;
+			pin_code[0] = '1';
+			pin_code[1] = '2';
+			pin_code[2] = '3';
+			pin_code[3] = '4';
+			esp_bt_gap_pin_reply(param->pin_req.bda, true, 4, pin_code);
+		}
+		break;
+	}
+
+#if (CONFIG_BT_SSP_ENABLED == true)
+	case ESP_BT_GAP_CFM_REQ_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %d", param->cfm_req.num_val);
+		esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
+		break;
+	case ESP_BT_GAP_KEY_NOTIF_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey:%d", param->key_notif.passkey);
+		break;
+	case ESP_BT_GAP_KEY_REQ_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
+		break;
+#endif
+
+	case ESP_BT_GAP_MODE_CHG_EVT:
+		ESP_LOGI(SPP_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode:%d", param->mode_chg.mode);
+		break;
+
+	default: {
+		ESP_LOGI(SPP_TAG, "event: %d", event);
+		break;
+	}
+	}
+	return;
+}
+
+void app_main(void)
+{
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( ret );
+
+	ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
+	esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+	if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_bluedroid_init()) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_bluedroid_enable()) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_bt_gap_register_callback(esp_bt_gap_cb)) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s gap register failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_spp_register_callback(esp_spp_cb)) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s spp register failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+	if ((ret = esp_spp_init(esp_spp_mode)) != ESP_OK) {
+		ESP_LOGE(SPP_TAG, "%s spp init failed: %s\n", __func__, esp_err_to_name(ret));
+		return;
+	}
+
+#if (CONFIG_BT_SSP_ENABLED == true)
+	/* Set default parameters for Secure Simple Pairing */
+	esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+	esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+	esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+#endif
+
+	/*
+	 * Set default parameters for Legacy Pairing
+	 * Use variable pin, input pin code when pairing
+	 */
+	esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_VARIABLE;
+	esp_bt_pin_code_t pin_code;
+	esp_bt_gap_set_pin(pin_type, 0, pin_code);
+
+	/* Create Queue */
+	xQueueCmd = xQueueCreate( 10, sizeof(CMD_t) );
+	configASSERT( xQueueCmd );
+
+	CMD_t cmdBuf;
+	while(1) {
+		xQueueReceive(xQueueCmd, &cmdBuf, portMAX_DELAY);
+		ESP_LOGI(MAIN_TAG,"cmdBuf.command=%d", cmdBuf.command);
+		if (cmdBuf.command == CMD_OPEN) {
+			ESP_LOGI(MAIN_TAG, "Connect to device");
+		} else if (cmdBuf.command == CMD_CLOSE) {
+			ESP_LOGI(MAIN_TAG, "disconnect from device");
+		} else if (cmdBuf.command == CMD_RECEIVE) {
+			ESP_LOGI(MAIN_TAG, "Your speek is [%s]", cmdBuf.payload);
+		}
+	}
+}
